83d6f39e55baf87af9a53eb6281be7e60514ff48
azinoVo <azinovo22@gmail.com>
2019-05-22 17:11:34 -0500

    minor changes

--- gigapet/src/components/SortDateForm.js
+++ gigapet/src/components/SortDateForm.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-import {getFoodByDate} from '../actions'
 
 class SortDateForm extends Component {
     constructor() {
--- gigapet/src/components/SortDateFormSpecific.js
+++ gigapet/src/components/SortDateFormSpecific.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-import {getFoodByRange} from '../actions';
 
 class SortDateFormSpecific extends Component {
     constructor() {
