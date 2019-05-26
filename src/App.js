1f9a068138d9295409613493086d70ad8af9e5f9
azinoVo <azinovo22@gmail.com>
2019-05-25 23:20:16 -0500

    cleaning up some code

--- src/App.js
+++ src/App.js
@@ -7,12 +7,8 @@ import Home from './components/Home';
 import TopBar from './components/TopBar';
 import AddEntry from './components/AddEntry';
 import ManageAccount from './components/ManageAccount';
-import History from './components/History';
 import PrivateRoute from './components/PrivateRoute';
 import PrivateRoute2 from './components/PrivateRoute2';
-import SortDateForm from './components/SortDateForm';
-import SortDateFormSpecific from './components/SortDateFormSpecific';
-import SortDateFormCategory from './components/SortDateFormCategory';
 import SortFormAdvanced from './components/SortFormAdvanced';
 import EditEntry from './components/EditEntry';
 import DragonLair from './components/DragonLair';
@@ -29,25 +25,14 @@ function App() {
         <PrivateRoute2 path="/add-entry" component={AddEntry}/>
         <PrivateRoute2 path="/manage-account" component={ManageAccount}/>
         <PrivateRoute2 path="/history" component={SortFormAdvanced}/>
-
-        {/* <PrivateRoute2 path="/history" component={History}/>
-        <PrivateRoute2 path="/history/day" component={SortDateForm}/>
-        <PrivateRoute2 path="/history/specific" component={SortDateFormSpecific}/>
-        <PrivateRoute2 path="/history/category" component={SortDateFormCategory}/>
-        <PrivateRoute2 path="/history/advanced" component={SortFormAdvanced}/> */}
-        
-        {/* <PrivateRoute2 path="/edit-entry" component={EditEntry}/> */}
-
         <Route path="/edit-entry" render={props => (<EditEntry {...props} stateRouter={props.location.state} />)}/>
         <PrivateRoute path="/secret" component={DragonLair}/>
-
 
       </Router>
     </div>
   );
 }
 
-// <Route path='/movies/:id' render={props => (<Movie {...props} addToSavedList={this.addToSavedList} />)} />
 
 export default App;
 
